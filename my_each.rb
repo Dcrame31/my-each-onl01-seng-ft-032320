131e3d7f7ffe8c9b57bb589f1fc582f9a0af75b2
Diane Crame <dcrame@icloud.com>
2020-03-25 17:39:28 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,21 @@
-def my_each # put argument(s) here
+def my_each(array) # put argument(s) here
+  # should work similarly to .each method
   # code here
+  if block_given?
+    i = 0
+    while i < array.length
+      yield(array[i])
+      i += 1
+    end
+  else
+    "No block was given."
+end
+end
+
+collection = [1, 2, 3, 4]  
+
+my_each([1, 2, 3, 4]) do |num| 
+  array = []
+  array << num[i]
+end
 end
